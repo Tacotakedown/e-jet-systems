37d7cd6df680f6ff108dbede4de3a567da7f5d28
Tacotakedown <robogod9@gmail.com>
2024-04-27 01:03:22 -0600

    Feat(Hydraulic); AC Pump constructor

---
+++ src/systems/hydraulic/pump/ac_motor_pump.rs
@@ -0,0 +1,24 @@
+#[derive(Debug)]
+pub struct AcMotorPump {
+    rated_output_flow_l_min: f64,
+    powered: bool,
+}
+impl AcMotorPump {
+    pub fn new(rated_output_flow_l_min: f64) -> Self {
+        Self {
+            rated_output_flow_l_min,
+            powered: false,
+        }
+    }
+    pub fn set_power_state(&mut self, state: bool) {
+        self.powered = state
+    }
+
+    pub fn get_output(&self) -> f64 {
+        if self.powered {
+            self.rated_output_flow_l_min
+        } else {
+            0.
+        }
+    }
+}
